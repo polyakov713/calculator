8b117e56d646bb8d99820d32cbf14bf2490b7e90
Vladislav Polyakov <vladislav.polyakov.1994@gmail.com>
2021-02-19 11:44:44 +0300

    Замена табличной верстки на гриды

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -5,66 +5,37 @@ import styles from './Buttons.css?module';
 
 @Component
 export default class Buttons extends VueComponent {
-  private buttons = {
-    rows: [
-      ['7', '8', '9', 'C'],
-      ['4', '5', '6', '-'],
-      ['1', '2', '3', '+'],
-    ],
-  };
+  private buttons = ['7', '8', '9', 'C', '4', '5', '6', '-', '1', '2', '3', '+'];
 
   onClick(event: MouseEvent) {
+    console.log(event.target);
     const buttonText: string = event.target ? (event.target as HTMLElement).innerHTML : '';
     this.$emit('buttonClick', buttonText);
   }
 
   render() {
     return (
-      <table class={styles.buttons}>
-        <thead></thead>
-        <tbody onClick={this.onClick}>
-          {
-            this.buttons.rows.map((row, index) => {
-              return (
-                <tr
-                  key={`tr-${index}`}
-                  class={styles.tr}
-                >
-                  {
-                    row.map(item => {
-                      return (
-                        <td
-                          key={`td-${item}`}
-                          class={styles.td}
-                        >
-                          <button class={styles.button}>
-                            {item}
-                          </button>
-                        </td>
-                      )
-                    })
-                  }
-                </tr>
-              )
-            })
-          }
-          <tr class={styles.tr}>
-            <td
-              colspan="3"
-              class={styles.td}
+      <div
+        class={styles.buttons}
+        onClick={this.onClick}
+      >
+        {
+          this.buttons.map((button) => (
+            <button
+              key={`button-${button}`}
+              class={styles.button}
             >
-              <button class={styles.button}>
-                0
-              </button>
-            </td>
-            <td class={styles.td}>
-              <button class={styles.button}>
-                =
-              </button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+              {button}
+            </button>
+          ))
+        }
+        <button class={`${styles.button} ${styles.zeroButton}`}>
+          0
+        </button>
+        <button class={styles.button}>
+          =
+        </button>
+      </div>
     )
   }
 }
